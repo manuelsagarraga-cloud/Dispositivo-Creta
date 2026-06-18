b010a57834afa9da6784fd0c517f755344313943
manuelsagarraga-cloud <manuel.sagarraga@gmail.com>
2026-06-17 21:45:17 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const SUPABASE_URL = 'https://quejmdetmxexqzjsrsbl.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://quejmdetmxexqzjsrsbl.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZWptZGV0bXhleHF6anNyc2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MzMzMzEsImV4cCI6MjA5NzMwOTMzMX0.YfOC4ePS1XKySdgyvZAwy514tyP6UDYT4dhYom8-vmM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
